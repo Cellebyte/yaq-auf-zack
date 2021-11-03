42b7ebe946cf9d473d7ce632420cc8cd0c4f4cc3
Marcel Fest <marcel.fest@live.de>
2021-11-03 23:07:33 +0100

    use external files to fix template issues

---
+++ assets/js/math.js
@@ -0,0 +1,17 @@
+MathJax = {
+  options: {
+    // Remove <code> tags from the blacklist. Even though we pass an
+    // explicit list of elements to process, this blacklist is still
+    // applied.
+    skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'annotation', 'annotation-xml'],
+    includeHtmlTags: {br: '\n', wbr: '', '#comment': '', 'code': '', 'pre': ''},
+    processHtmlClass: 'language-math',
+  },
+  tex: {
+    // By default, only \( is enabled for inline math, to prevent false
+    // positives. Since we already only process code blocks that contain
+    // exactly one math expression and nothing else, it is also fine to
+    // use the nicer $...$ construct for inline math.
+    inlineMath: { '[+]': [['$', '$']] },
+  },
+};
